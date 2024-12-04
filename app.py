e221f3c3a7e73aaeda38c69d68a2f9570cfe3f71
sabdukodirov <abdukodirovsamandar3@gmail.com>
2024-12-04 17:32:27 +0500

    add home and projects page

--- app.py
+++ app.py
@@ -2,19 +2,21 @@ from flask import Flask, render_template, session
 from article import Article
 
 app = Flask(__name__)
-app.secret_key = "thisissomethingsecure" 
+app.secret_key = "thisissomethingsecure"
 
 articles = Article.all()
 
-@app.route("/")
+
+@app.route("/articles")
 def blog():
-	return render_template("blog.html", articles=articles)
+    return render_template("blog.html", articles=articles)
 
 
 @app.route("/set-session")
 def set_session():
     session['user_id'] = 1
     return "session set"
+
 
 @app.route("/get-session")
 def get_session():
@@ -26,12 +28,23 @@ def get_session():
 @app.route("/admin")
 def admin():
     return render_template("login.html")
-    
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
 
 @app.route("/blog/<slug>")
 def article(slug: str):
     article = articles[slug]
     return render_template("article.html", article=article)
+
 
 if __name__ == "__main__":
     app.run(port=4200, debug=True)
